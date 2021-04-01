f2e9119df48d8b6dd3b4e20ded76a1e726c1b886
Doogii <doogiitsogtoo08@gmail.com>
2021-04-01 20:49:06 +0800

    first

---
+++ jquery.js
@@ -0,0 +1,29 @@
+ $(document).ready(function(){
+     $(".click").click(function(){
+       $("green").animate({left: '30px'});
+     });
+   });
+
+ $(document).ready(function(){
+    $(".click").click(function(){
+      $(".yellow").animate({left: '150px'});
+    });
+  });  
+
+ $(document).ready(function(){
+    $(".click").click(function(){
+      $(".black").animate({left: '250px'});
+    });
+  });  
+
+  $(document).ready(function(){
+    $(".click").click(function(){
+      $(".blue").animate({left: '350px'});
+    });
+  });  
+
+  $(document).ready(function(){
+    $(".click").click(function(){
+      $(".red").animate({left: '450px'});
+    });
+  });  
