af18db58c087bad08c06007a144e4d5cc41d9191
doogiitsogtoo <doogiitsogtoo08@gmail.com>
2021-04-05 09:45:57 +0800

    duud1

---
+++ jquery.js
@@ -0,0 +1,29 @@
+ $(document).ready(function(){
+     $(".click").click(function(){
+       $("green").animate({left: '30px'});
+     });
+   });
+
+ $(document).ready(function(){
+    $(".click").click(function(){
+      $(".yellow").animate({left: '210px'});
+    });
+  });  
+
+ $(document).ready(function(){
+    $(".click").click(function(){
+      $(".black").animate({left: '310px'});
+    });
+  });  
+
+  $(document).ready(function(){
+    $(".click").click(function(){
+      $(".blue").animate({left: '410px'});
+    });
+  });  
+
+  $(document).ready(function(){
+    $(".click").click(function(){
+      $(".red").animate({left: '510px'});
+    });
+  });  
---
+++ service_jquery.js
@@ -0,0 +1,10 @@
+const spaceBtn = document.querySelector("#space");
+const display = document.querySelector(".display");
+
+function disp(result) {
+    form.textarea.value = form.textarea.value + result;
+
+    spaceBtn.onclick = (() => {
+        form.textarea.value += " ";
+    });
+}   
